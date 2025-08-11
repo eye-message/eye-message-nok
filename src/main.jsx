5e691df93ca5d936eaffff99f8764f139acfb716
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-08-12 01:34:26 +0900

    refactor: restructure with vite and seperate route

--- src/main.jsx
+++ src/main.jsx
@@ -13,22 +13,27 @@ import Add from "./pages/Add";
 import CalendarPage from "./pages/CalendarPage";
 import Settings from "./pages/Settings";
 import Login from "./pages/Login";
-import reportWebVitals from "./utils/reportWebVitals";
-import "./styles/App.css";
 import Layout from "./pages/Layout";
+
+import "./styles/App.css";
+import Signup from "./pages/Signup";
+import MessageAddForm from "./pages/MessageAddForm";
+import MessageList from "./pages/MessageList";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/notification" replace />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<Layout />}>
+          {/* <Route index element={<Navigate to="/notification" replace />} /> */}
           <Route path="/notification" element={<Notification />} />
           <Route path="/board" element={<Board />} />
-          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<MessageList />} />
+          <Route path="/add" element={<MessageAddForm />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </Router>
@@ -40,5 +45,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-
-reportWebVitals();
